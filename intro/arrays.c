50822bedd9cadd423844bed132a1623c1ed1d1a4
Diego <dglvr3@gmail.com>
2026-06-29 20:42:36 -0300

    feat: add function pointer example

--- intro/arrays.c
+++ intro/arrays.c
@@ -3,15 +3,27 @@
 #include <stdlib.h>
 #include "../lib/customlib.h"
 
-static const int ARR_SIZE = 3;
+static const size_t ARR_SIZE = 3;
+
+// Function pointers notation:
+// return_type (*function_name)(type_param1, type_param2, ...)
+void map(int *arr, size_t arr_size, int (*transform)(int)) {
+    for (size_t i = 0; i < arr_size; i++) {
+        arr[i] = transform(arr[i]);
+    }
+}
+
+int multiply_by_two(int value) {
+    return value * 2;
+}
 
 int main() {
     int arr[] = {1, 2, 3};
 
-    // int first = arr[0]; // arr[0] -> *(arr + 0)
-    // int second = arr[1]; // arr[1] -> *(arr + 1)
-    // int third = arr[2]; // arr[2] -> *(arr + 2)
-
+    printf("Before: ");
+    print_array(arr, ARR_SIZE);
+    map(arr, ARR_SIZE, multiply_by_two);
+    printf("After:  ");
     print_array(arr, ARR_SIZE);
 
     return EXIT_SUCCESS;
