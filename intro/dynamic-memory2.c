10ad61d8a45f7d3a40f33edc7b3580f39c52cf6e
Diego <dglvr3@gmail.com>
2026-07-10 09:48:12 -0300

    feat: add exercises to calculate the string length and to reverse it in place

--- intro/dynamic-memory2.c
+++ intro/dynamic-memory2.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int add(int a, int b) { return a + b; }
+size_t my_strlen(char *string) {
+	char *p = string;
+	while (*p != '\0') p++;
+	return p - string;	
+}
 
-int subtract(int a, int b) { return a - b; }
+void reverse_string(char *string) {
+	size_t length = my_strlen(string);
+	if (length == 0) return;
+	
+	size_t left = 0;
+	size_t right = length - 1;
+	for (size_t i = 0; i < length/2; i++) {
+		char temp = string[left];
+		string[left] = string[right];
+		string[right] = temp;
+		left++;
+		right--;
+	}
+}
 
 int main() {
-	int x = 15, y = 5;
-	int (*operation)(int, int);
-
-	operation = add;
-	printf("\nOperation: ADD | Result: %d\n", operation(x,y));
-	
-	operation = subtract;
-	printf("Operation: SUBTRACT | Result: %d\n", operation(x,y));
-
+	char string[] = "Diego";
+	printf("Before: %s\n", string);
+	reverse_string(string);
+	printf("After: %s\n", string);
+		
 	return EXIT_SUCCESS;
 }
+
