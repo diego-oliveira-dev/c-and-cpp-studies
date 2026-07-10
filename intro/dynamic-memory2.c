0845ab394b1f798322f25b5b1fdd85fea160e5ec
Diego <dglvr3@gmail.com>
2026-07-10 08:28:57 -0300

    feat: add function pointer exercise

--- intro/dynamic-memory2.c
+++ intro/dynamic-memory2.c
@@ -1,29 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int add(int a, int b) { return a + b; }
+
+int subtract(int a, int b) { return a - b; }
+
 int main() {
-	int capacity = 2, count = 0;
-	int *arr = malloc(capacity * sizeof(int));
+	int x = 15, y = 5;
+	int (*operation)(int, int);
 
-	printf("\nInitial capacity: %d\n", capacity);
-
-	for (size_t i = 0; i < 5; i++) {
-		if (count == capacity - 1) {
-			capacity *= 2;
-			int *temp = realloc(arr, capacity * sizeof(int));
-			if (temp != NULL) arr = temp;
-			printf("Capacity doubled to %d..\n", capacity);
-		}	
-
-		arr[i] = ((i + 1) * 10);
-		count++;
-	}
-
-	printf("Final data: \n");
-	for (size_t i = 0; i < count; i++) printf("arr[%ld] = %d\n", i, arr[i]);
-	printf("\nFinal capacity: %d\n", capacity);
-
-	free(arr);
+	operation = add;
+	printf("\nOperation: ADD | Result: %d\n", operation(x,y));
+	
+	operation = subtract;
+	printf("Operation: SUBTRACT | Result: %d\n", operation(x,y));
 
 	return EXIT_SUCCESS;
 }
