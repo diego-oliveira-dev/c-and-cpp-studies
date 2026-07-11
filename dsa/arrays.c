66fdaf65a6173a049281709575ccdb2d008632e7
Diego <dglvr3@gmail.com>
2026-07-11 16:31:49 -0300

    feat: add solution to Contains Duplicate problem

--- dsa/arrays.c
+++ dsa/arrays.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../lib/customlib.h"
+#include <stdbool.h>
 
 void runningSum(int *nums, size_t nums_size) {
     // Time Complexity -> O(n)
@@ -11,17 +11,38 @@ void runningSum(int *nums, size_t nums_size) {
     }
 }
 
+int compare_ints(const void *a, const void *b) {
+	const int *x = a;
+	const int *y = b;
+	if (*x < *y) return -1;
+	if (*x > *y) return 1;
+	return 0;
+}
+
+bool contains_duplicate(int *nums, size_t nums_size) {
+	if (nums_size == 1) return false;
+	size_t left = 0;
+	size_t right = 1;
+	qsort(nums, nums_size, sizeof(int), compare_ints);
+	while (right < nums_size) {
+		if (nums[left] == nums[right]) return true;
+		left++;
+		right++;
+	}
+	return false;
+}
+
+
 int main() {
-    int nums[] = {1,2,3,4};
-    size_t nums_size = sizeof(nums) / sizeof(nums[0]);
-    
-    runningSum(nums, nums_size);
+	int nums[] = {1, 2, 3, 1};
+	size_t nums_size = sizeof(nums) / sizeof(nums[0]);
+	
+	printf("Input: [ ");
+	for (size_t i = 0; i < nums_size; i++) printf("%d ", nums[i]);
+	printf("]\n");
 
-    puts("\nInput: nums = [1,2,3,4]\n");
-    puts("Expected output: [1,3,6,10]");
-    printf("Actual output:   ");
-    print_array(nums, nums_size);
-    puts("");
+   	bool result = contains_duplicate(nums, nums_size);
+	printf("Result: %s\n", result ? "true" : "false");
 
-    return EXIT_SUCCESS;
+    	return EXIT_SUCCESS;
 }
